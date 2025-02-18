45a550c714b4d637e53697d39ea0c503d5a3a07c
TrevorProg <discordtrevor7@gmail.com>
2025-02-18 17:51:04 +0300

    Add files via upload

---
+++ Loops/loops.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for even in numbers:
+    if even % 2 == 1:
+        print(even)
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for even in numbers:
+    if even % 2 == 2:
+        print(even)
