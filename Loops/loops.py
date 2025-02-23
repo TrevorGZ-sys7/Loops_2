04b6e7d66e069faed349d271d77c62714ce8a464
TrevorProg <discordtrevor7@gmail.com>
2025-02-23 15:20:17 +0300

    Update loops.py

--- Loops/loops.py
+++ Loops/loops.py
@@ -1,11 +1,9 @@
 numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 for even in numbers:
-    if even % 2 == 1:
+    if even % 2 == 0:
         print(even)
 
-numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-for even in numbers:
-    if even % 2 == 2:
-        print(even)
+for odd in numbers:
+    if odd % 2 == 1:
+        print(odd)
